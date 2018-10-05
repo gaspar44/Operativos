cb31d0be30bffbc44745c2b256d7696d5406748f
Gaspar Hermán <gaspar.herman95@gmail.com>
2018-10-05 19:51:38 +0200

    Prototipo de la opcion1

--- Practica1/opcion1.sh
+++ Practica1/opcion1.sh
@@ -5,6 +5,7 @@ echo "-----------"
 
 # Leemos los archivos
 IFS=$'\n' registers=($(cat "datos/geoplaces2.csv"))
+declare -a max=()
 
 for (( i = 1; i < ${#registers[@]}; i++ )); 
 do
@@ -18,10 +19,54 @@ do
 	readedPrice=$(echo ${registers[$i]} | cut -d ',' -f 16)
 
 	for item in "${alcohol[@]}"; do
-		if [[ $readedAlcohol == ${alcohol[$item]} ]];then
+		if [[ $readedAlcohol == ${item} ]];then
 			promedio=$(bc -l <<< $promedio+$pesoAlcohol)
+			break
 		fi
 	done
 
+	for item in "${price[@]}"; do
+		if [[ $readedPrice == ${item} ]];then
+			promedio=$(bc -l <<< $promedio+$pesoPrice)
+			break
+		fi
+	done
+
+	for item in "${smokingArea[@]}"; do
+		if [[ $readedAllowedSmoking == ${item} ]];then
+			promedio=$(bc -l <<< $promedio+$pesoSmoingArea)
+			break
+		fi
+	done
+
+	for item in "${dressCode[@]}"; do
+		if [[ $readedDressCode == ${item} ]];then
+			promedio=$(bc -l <<< $promedio+$pesoDressCode)
+			break
+		fi
+	done
+
+	for item in "${accessibility[@]}"; do
+		if [[ $readedAccessibility == ${accessibility[$item]} ]];then
+			promedio=$(bc -l <<< $promedio+$pesoAccessibility)
+			break
+		fi
+	done
+
+	if [[ $promedio -gt $promedioMax ]]; then
+		max=${registers[$i]}
+
+	else if [[ $promedio -eq $promedioMax ]];then
+		max+=${registers[$i]}
+	fi
 done
+
+if [[ ${#max[@]} == 1 ]]; then
+	echo $max
+
+else 
+	elementToShow=$(($RANDOM % ${#max[@]}))
+	echo ${max[$elementToShow]}
+fi
+
 $@
